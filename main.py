fd9e15fb3039ec11e7e875c492c99b0cb4e36315
MateuszSleziak <sleziak2006@gmail.com>
2024-03-03 19:48:10 +0100

    new function edit_column_names

--- main.py
+++ main.py
@@ -16,3 +16,33 @@ def get_new_column_names(file_path):
     except FileNotFoundError:
         print("The specified file does not exist.")
         return None
+
+def edit_column_names(csv_file, new_names):
+    try:
+        data = pd.read_csv(csv_file)
+        
+        print("Current column names:")
+        print(data.columns)
+        
+        if len(new_names) != len(data.columns):
+            print("The number of new column names must be the same as the number of current columns.")
+            return None
+
+        for i, new_name in enumerate(new_names):
+            if new_name is not None:
+                data.rename(columns={data.columns[i]: new_name}, inplace=True)
+        
+        output_file = input("CSV file name to which you want to save the changes: ")
+        data.to_csv(output_file, index=False)
+        print("Column names have been updated and saved to the CSV file.")
+    except FileNotFoundError:
+        print("The specified file does not exist.")
+
+if __name__ == "__main__":
+    csv_file = input("Please provide the name of the CSV file: ")
+    new_names_file = input("Please provide the name of the file containing the new column names: ")
+    
+    new_names = get_new_column_names(new_names_file)
+    
+    if new_names:
+        edit_column_names(csv_file, new_names)
