9fd444569caa3108257c3c3d76920850722900ff
MateuszSleziak <sleziak2006@gmail.com>
2024-03-03 18:53:55 +0100

    new function file_name

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pandas as pd 
 
-csv = input("CSV file path: ")
+def file_name():
+    csv = input("CSV File Name (File Must Be in the Same Folder as the Program):  ")
 
-try:
-    data = pd.read_csv(csv)
-except FileNotFoundError:
-    print("The specified file does not exist.")
+    try:
+        data = pd.read_csv(csv)
+    except FileNotFoundError:
+        print("The specified file does not exist.")
+
+file_name()
+    
+
+    
 
