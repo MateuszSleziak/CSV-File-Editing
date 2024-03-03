41cc5b1e1cdea6b7c187ea6ba37970617432c6c3
MateuszSleziak <sleziak2006@gmail.com>
2024-03-03 19:34:09 +0100

    new function get_new_column_names

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import pandas as pd 
+import pandas as pd
 
-def file_name():
-    csv = input("CSV File Name (File Must Be in the Same Folder as the Program):  ")
-
+def get_new_column_names(file_path):
     try:
-        data = pd.read_csv(csv)
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            new_names = []
+            for line in lines:
+                parts = line.split(": ")
+                if len(parts) > 1:
+                    new_name = parts[1].strip()
+                    new_names.append(new_name)
+                else:
+                    new_names.append(None)
+        return new_names
     except FileNotFoundError:
         print("The specified file does not exist.")
-
-file_name()
-    
-
-    
-
+        return None
