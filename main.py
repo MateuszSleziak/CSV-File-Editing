0d80791129f6dac9c13ffa2c6b40b7a9cb62a715
Mateusz Śleziak <sleziak2006@gmail.com>
2024-03-05 13:16:29 +0100

    new function - translate_header_by_dictionary / translate_header_by_dictionary

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_new_column_names(file_path):
             lines = file.readlines()
             new_names = []
             for line in lines:
-                parts = line.split(": ")
+                parts = line.split(":")
                 if len(parts) > 1:
                     new_name = parts[1].strip()
                     new_names.append(new_name)
@@ -18,8 +18,7 @@ def get_new_column_names(file_path):
         print("The specified file does not exist.")
         return None
 
-def edit_column_names(csv_file, new_names):
-    translator = Translator()
+def translate_header_by_dictionary(csv_file, new_names):
     try:
         data = pd.read_csv(csv_file)
         
@@ -32,8 +31,7 @@ def edit_column_names(csv_file, new_names):
 
         for i, new_name in enumerate(new_names):
             if new_name is not None:
-                translator_name = translator.translate(new_name, src='auto', dest = 'en')
-                data.rename(columns={data.columns[i]: translator_name}, inplace=True)
+                data.rename(columns={data.columns[i]: new_name}, inplace=True)
         
         output_file = input("CSV file name to which you want to save the changes: ")
         data.to_csv(output_file, index=False)
@@ -41,12 +39,39 @@ def edit_column_names(csv_file, new_names):
     except FileNotFoundError:
         print("The specified file does not exist.")
 
+def translate_header_by_google(csv_file, new_names):
+    translator = Translator()
+    try:
+        data = pd.read_csv(csv_file)
+        
+        print("Current column names:")
+        print(data.columns)
+        
+        if len(new_names) != len(data.columns):
+            print("The number of new column names must be the same as the number of current columns.")
+            return None
+        
+        translated_columns = []
+        for old_name in data.columns:
+            translated_name =translator.translate(old_name, src = 'auto', dest = 'pl').text
+            translated_columns.append(translated_name)
+        data.columns = translated_columns 
+
+        output_file = input("CSV file name to which you want to save the changes: ")
+        data.to_csv(output_file, index=False)
+        print("Column names have been updated and saved to the CSV file.")
+    except FileNotFoundError:
+        print("The specified file does not exist.")
+
 if __name__ == "__main__":
-    csv_file = input("Please provide the name of the CSV file: ")
-    new_names_file = input("Please provide the name of the file containing the new column names: ")
+    csv_file = input(" filPlease provide the name of the CSVe: ")
+    # new_names_file = input("Please provide the name of the file containing the new column names: ")
+    # new_names_file = pd.read_csv("dictionary.csv")
+    new_names_file = "dictionary.txt"
     
     new_names = get_new_column_names(new_names_file)
     
+
     if new_names:
-        edit_column_names(csv_file, new_names)
+       translate_header_by_dictionary(csv_file, new_names)
 #MŚ
