35a215d4392f1a5bd40ac2ea1e65eff1ddec60c7
Mateusz Śleziak <sleziak2006@gmail.com>
2024-03-06 11:01:06 +0100

    translate column by google

--- main.py
+++ main.py
@@ -39,39 +39,28 @@ def translate_header_by_dictionary(csv_file, new_names):
     except FileNotFoundError:
         print("The specified file does not exist.")
 
-def translate_header_by_google(csv_file, new_names):
-    translator = Translator()
+def translate_column_by_google(csv_file, output_file, column_index, source_lang='pl', target_lang='en'):
     try:
         data = pd.read_csv(csv_file)
         
-        print("Current column names:")
-        print(data.columns)
+        if column_index < 0 or column_index >= len(data.columns):
+            print("Invalid column index.")
+            return
         
-        if len(new_names) != len(data.columns):
-            print("The number of new column names must be the same as the number of current columns.")
-            return None
+        column_name = data.columns[column_index]
         
-        translated_columns = []
-        for old_name in data.columns:
-            translated_name =translator.translate(old_name, src = 'auto', dest = 'pl').text
-            translated_columns.append(translated_name)
-        data.columns = translated_columns 
-
-        output_file = input("CSV file name to which you want to save the changes: ")
+        translator = Translator()
+        data[column_name] = data[column_name].apply(lambda x: translator.translate(x, src=source_lang, dest=target_lang).text)
+        
         data.to_csv(output_file, index=False)
-        print("Column names have been updated and saved to the CSV file.")
+        print(f"Column at index {column_index} has been translated and saved to the CSV file '{output_file}'.")
+    
     except FileNotFoundError:
         print("The specified file does not exist.")
 
 if __name__ == "__main__":
-    csv_file = input(" filPlease provide the name of the CSVe: ")
-    # new_names_file = input("Please provide the name of the file containing the new column names: ")
-    # new_names_file = pd.read_csv("dictionary.csv")
-    new_names_file = "dictionary.txt"
+    csv_file = input("Please provide the name of the CSV file: ")
+    output_file = input("CSV file name to which you want to save the changes: ")
+    column_index = int(input("Please provide the index of the column you want to translate: "))
     
-    new_names = get_new_column_names(new_names_file)
-    
-
-    if new_names:
-       translate_header_by_dictionary(csv_file, new_names)
-#MŚ
+    translate_column_by_google(csv_file, output_file, column_index)
